b432260d8b696f749e57d7e882882f65e61021e5
Vipul Dhaigude <vipuldhaigude@gmail.com>
2019-09-30 11:04:34 +0200

    SQUAD-9-313 Corrected caching error in delete

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -3,51 +3,51 @@ const validator = require('../helpers/validator');
 const cacher = require('../helpers/cacher');
 
 class clientsController {
-	// GET - Returns a list of clients
-	static async get() {
-		return ClientModel.getList();
-	}
+    // GET - Returns a list of clients
+    static async get() {
+        return ClientModel.getList();
+    }
 
-	// GET - Returns one client by ID
-	static async getOne(req) {
-		const {
-			clientId,
-		} = req.params;
-		return ClientModel.getOne(clientId);
-	}
+    // GET - Returns one client by ID
+    static async getOne(req) {
+        const {
+            clientId,
+        } = req.params;
+        return ClientModel.getOne(clientId);
+    }
 
-	// POST - Create a client
-	static async createOne(req) {
-		await validator.validate('ClientModel', req.body);
+    // POST - Create a client
+    static async createOne(req) {
+            await validator.validate('ClientModel', req.body);
 
-		const payloadHash = ClientModel.hashClient(req.body);
-		if (cacher.isCached(payloadHash)) return cacher.getCached(payloadHash);
-		const client = await ClientModel.createOne(req.body, payloadHash);
-		await cacher.cacheAdd(payloadHash, client);
-		return client;
-	}
-	// PATCH - Update a client
-	static async updateOne(req) {
-		await validator.validate('ClientPatchModel', req.body);
-		const {
-			clientId,
-		} = req.params;
-		const client = await ClientModel.updateOne(clientId, req.body);
-		return client;
-	}
+            const payloadHash = ClientModel.hashClient(req.body);
+            if (cacher.isCached(payloadHash)) return cacher.getCached(payloadHash);
+            const client = await ClientModel.createOne(req.body, payloadHash);
+            await cacher.cacheAdd(payloadHash, client);
+            return client;
+        }
+        // PATCH - Update a client
+    static async updateOne(req) {
+        await validator.validate('ClientPatchModel', req.body);
+        const {
+            clientId,
+        } = req.params;
+        const client = await ClientModel.updateOne(clientId, req.body);
+        return client;
+    }
 
-	// DELETE - Delete a client
-	static async deleteOne(req) {
-		const {
-			clientId,
-		} = req.params;
-		const client = await ClientModel.getOne(clientId);
-		await ClientModel.deleteById(clientId);
-		if (cacher.isCached(client.payloadHash)) cacher.cache.delete(client.payloadHash);
-		return {
-			message: 'success',
-		};
-	}
+    // DELETE - Delete a client
+    static async deleteOne(req) {
+        const {
+            clientId,
+        } = req.params;
+        const client = await ClientModel.getOne(clientId);
+        await ClientModel.deleteById(clientId);
+        if (cacher.isCached(client.payloadHash)) cacher.cache.delete(client.payloadHash);
+        return {
+            message: 'success',
+        };
+    }
 }
 
 
